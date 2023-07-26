77e50b102ad353b9b02860dc4c3e0b4b0cebd53e
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-26 23:21:50 +0500

    Add files via upload

---
+++ circle.dart
@@ -0,0 +1,11 @@
+// Write dart program to create circle shape with steric(*)
+
+void main(){
+  print("              *************"       );
+  print("         *                     *"   );
+print("          *                       *"  );
+  print("        *                       *"  );
+   print("       *                       *"   );
+    print("         *                  *"     );
+  print("               ************"        );
+}
