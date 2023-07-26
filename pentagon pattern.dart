51968e06044dd786ddb4f7d249b5759e0a817bbe
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-26 23:54:41 +0500

    Add files via upload

---
+++ pentagon pattern.dart
@@ -0,0 +1,16 @@
+// Write dart program to create pentagon shape with steric(*)
+
+void main(){
+  print("                       *                                  ");
+  print("                   *       *                              ");
+  print("                *             *                           ");
+  print("             *                   *                        ");
+  print("          *                         *                     ");
+  print("       *                              *                   ");
+  print("        *                            *                    ");
+  print("         *                          *                     ");
+  print("          *                        *                      ");
+  print("           *                      *                       ");
+  print("            *                    *                        ");
+  print("             ********************                         ");
+}
