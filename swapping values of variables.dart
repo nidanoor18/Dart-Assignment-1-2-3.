c2debae9635b0ffdd371e3ac4971fbd573641de1
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-27 22:04:22 +0500

    Add files via upload

---
+++ swapping values of variables.dart
@@ -0,0 +1,12 @@
+void main(){
+  //Before Swapping values of varaibles
+var num1=10,num2=30,temp;
+  temp=num1;
+  num1=num2;
+  num2=temp;
+  //after Swapping values of varaibles
+  print(num1);
+  print(num2);
+}
+
+  
