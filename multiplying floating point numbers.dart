71e6490776ce512dffa351fd8f62e4e50c4597ed
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-27 22:45:51 +0500

    Add files via upload

---
+++ multiplying floating point numbers.dart
@@ -0,0 +1,11 @@
+//Write a program to multiply two floating point numbers
+void main(){
+  double num1,num2,result;
+  num1=65.78;
+  num2=23.76;
+  result=num1*num2;
+  //The result of num1,num2 is printed below
+  print(result);
+  }
+
+  
