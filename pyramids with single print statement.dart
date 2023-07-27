c31719cac8a347dc5c293e8374303edbc8b3253c
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-27 22:47:39 +0500

    Update pyramids with single print statement.dart

--- pyramids with single print statement.dart
+++ pyramids with single print statement.dart
@@ -1,3 +1,4 @@
+//Write a program to print pyramid pattern using backslash(\n) and single print() statement.
 void main(){
     //printing pyramids using single print statement and \n (back slash)
     print( "      *\n     *** \n    *****    \n   *******     \n  *********    \n ***********    ");
