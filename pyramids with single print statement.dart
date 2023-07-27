2257daf45d834a0812909507840c6ae89ebc1b72
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-27 21:08:12 +0500

    Add files via upload

---
+++ pyramids with single print statement.dart
@@ -0,0 +1,4 @@
+void main(){
+    //printing pyramids using single print statement and \n (back slash)
+    print( "      *\n     *** \n    *****    \n   *******     \n  *********    \n ***********    ");
+}
