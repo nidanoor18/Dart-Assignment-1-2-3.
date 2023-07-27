257248dd90bf9bafe428d0f7474c548aac80f458
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-27 23:05:10 +0500

    Add files via upload

---
+++ performing all arithmetic operations.dart
@@ -0,0 +1,28 @@
+//Write a program to perform all arithmetic operations.
+void main(){
+ double num1,num2,result;
+  num1=10.6;
+  num2=5.0;
+  //using addition operator 
+  result=num1+num2;
+  print('The sum of num1,num2:');
+  print(result);
+  //using subtraction operator 
+  result=num1-num2;
+  print('The difference of num1,num2');
+  print(result);
+  //using multiplication operator
+  result=num1*num2;
+  print('The product of num1,num2:');
+  print(result);
+   //using division operator
+  result=num1/num2;
+  print('The quotient of num1,num2:');
+  print(result);
+   //using modulus operator
+  result=num1%num2;
+  print('The modulus of num1,num2:');
+  print(result);
+  }
+
+  
