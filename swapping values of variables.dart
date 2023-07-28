1b241d60f43ff520b7a345d80e799d3e27dca19b
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-28 20:39:45 +0500

    Update swapping values of variables.dart

--- swapping values of variables.dart
+++ swapping values of variables.dart
@@ -1,8 +1,8 @@
 //Write a program to swap values of variables using temporary varaible(third varaiable).
 void main(){
   //Before Swapping values of varaibles
-/*     In Dart, the var keyword is used to declare variables without explicitly specifying their data type.Instead of 
-	explicitly stating the data type,Dart infers the type of the variable based on the value assigned to it.     */
+  /*  In Dart, the var keyword is used to declare variables without explicitly specifying their data type.Instead of 
+     explicitly stating the data type,Dart infers the type of the variable based on the value assigned to it.  */
   // Using var to declare another variable without specifying the Data Type.
 var num1=10,num2=30,temp;
   temp=num1;
