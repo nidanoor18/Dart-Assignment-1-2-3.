2cba9f7367eb709084bd4061f1315beebf40846a
Nida Noor <140308126+nidanoor18@users.noreply.github.com>
2023-07-28 20:34:12 +0500

    Add files via upload

---
+++ temperature conversion.dart
@@ -0,0 +1,17 @@
+//Write a program to convert celsius to fahrenheit.
+void main(){
+  //temperature in Celsius
+ double Celsius=25;
+ //Conversion from Celsius to Fahrenheit
+    double Fahrenheit = (Celsius )* (9 / 5) + 32;
+    
+    /*In Dart, the dollar sign ($) is used to put the value of variables or 
+    the result of calculations directly into a text (string). It's like a
+   placeholder that gets replaced with the actual value when the program runs.*/
+   
+    //dispalying temperature in fahrenheit
+     print("$Celsius Celsius is equal to $Fahrenheit Fahrenheit.");
+}
+/* To convert Fahrenheit into Celsius:
+ celsius = (fahrenheit - 32) * 5 / 9;     */
+  
